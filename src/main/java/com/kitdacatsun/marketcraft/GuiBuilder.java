c040d384acddaec7509bcddcf488043be968019a
Jamie Kimber <jamiekimber@btinternet.com>
2021-02-15 13:00:16 +0000

    Gui interface

--- src/main/java/com/kitdacatsun/marketcraft/GuiBuilder.java
+++ src/main/java/com/kitdacatsun/marketcraft/GuiBuilder.java
@@ -8,6 +8,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 
 
 public class GuiBuilder{
@@ -30,8 +31,8 @@ public class GuiBuilder{
     }
 
 
-    public void openInventory(Player player, String material, int amount) {
-        Inventory Bank = this.createGui("Shop", 27);
+    public void openInventory(Player player, String material, String name, int amount, String position , String originalMaterial, String originalName, int originalAmount) {
+        Inventory bank = this.createGui("Shop", 27);
 
         int cost = 10;
         ItemStack sell1 = build(Material.RED_STAINED_GLASS_PANE,ChatColor.RED + "Sell 1",1, "Shop");
@@ -40,23 +41,34 @@ public class GuiBuilder{
         ItemStack buy1 = build(Material.GREEN_STAINED_GLASS_PANE,ChatColor.GREEN + "Buy 1",1, "Shop");
         ItemStack buy10 = build(Material.GREEN_STAINED_GLASS_PANE,ChatColor.GREEN + "Buy 10",10, "Shop");
         ItemStack buy64 = build(Material.GREEN_STAINED_GLASS_PANE,ChatColor.GREEN + "Buy 64",64, "Shop");
-        ItemStack back = build(Material.RED_DYE,"Back",1);
+        ItemStack back = build(Material.RED_DYE,"Back",1,"Click to return to menu");
+        ItemStack middle = null;
+        ItemStack selector = null;
 
 
-        ItemStack middle = build(Material.getMaterial(material),Material.getMaterial(material).getName() ,amount);
+        if (position.equals("center")) {
+            middle = build(Material.getMaterial(material), name, amount, "Selected Item");
+            selector = build(Material.getMaterial(originalMaterial), originalName, originalAmount, "Click to confirm");
 
-        Bank.setItem(11, sell1);
-        Bank.setItem(10, sell10);
-        Bank.setItem(9, sell64);
-        Bank.setItem(13, middle);
-        Bank.setItem(15, buy1);
-        Bank.setItem(16, buy10);
-        Bank.setItem(17, buy64);
-        Bank.setItem(4, back);
+        }else {
+            selector = build(Material.getMaterial(material), name, amount, "Click to confirm");
+            middle = build(Material.getMaterial(originalMaterial),originalName,originalAmount,"Selected Item");
+        }
 
 
+        bank.setItem(11, sell1);
+        bank.setItem(10, sell10);
+        bank.setItem(9, sell64);
+        bank.setItem(15, buy1);
+        bank.setItem(16, buy10);
+        bank.setItem(17, buy64);
+        bank.setItem(4, back);
+        bank.setItem(13, middle);
+        bank.setItem(22, selector);
 
-        player.openInventory(Bank);
+
+
+        player.openInventory(bank);
 
     }
 }
--- src/main/java/com/kitdacatsun/marketcraft/GuiListener.java
+++ src/main/java/com/kitdacatsun/marketcraft/GuiListener.java
@@ -1,6 +1,7 @@
 package com.kitdacatsun.marketcraft;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,16 +25,38 @@ public class GuiListener implements Listener {
 
                 Player player = (Player) event.getWhoClicked();
 
-
                 int cost = 10;
 
-                if(clickedItem.getLore().getType.equals("Null")) {
+                if(clickedItem.getLore() != null) {
+                    if (clickedItem.getItemMeta().getLore().toString().equals("[Shop]")){
+                        GuiBuilder bank = new GuiBuilder();
+                        if (event.getView().getItem(13) != null){
+                            String originalMaterial = event.getView().getItem(13).getType().toString();
+                            String originalName = event.getView().getItem(13).getItemMeta().getDisplayName();
+                            int originalAmount = event.getView().getItem(13).getAmount();
+                            bank.openInventory(player, clickedItem.getType().toString(),clickedItem.getItemMeta().getDisplayName(), clickedItem.getAmount(),"selector",originalMaterial,originalName,originalAmount);
 
-                    GuiBuilder Bank = new GuiBuilder();
-                    Bank.openInventory(player, clickedItem.getType().toString(), 1);
+                        }else{
+                            bank.openInventory(player, clickedItem.getType().toString(),clickedItem.getItemMeta().getDisplayName(), clickedItem.getAmount(),"selector","BARRIER","",0);
+                        }
+                    }else{
+                        //return to previous menu
+                    }
 
                 } else {
-                    //blank for now lol
+
+                    GuiBuilder bank = new GuiBuilder();
+                    if (event.getView().getItem(22) != null){
+                        String originalMaterial = event.getView().getItem(22).getType().toString();
+                        String originalName = event.getView().getItem(22).getItemMeta().getDisplayName();
+                        int originalAmount = event.getView().getItem(22).getAmount();
+
+                        bank.openInventory(player, clickedItem.getType().toString(),clickedItem.getItemMeta().getDisplayName(), 1,"center",originalMaterial,originalName,originalAmount);
+
+                    }else{
+                        bank.openInventory(player, clickedItem.getType().toString(),clickedItem.getItemMeta().getDisplayName(), 1,"center","BARRIER","",0);
+                    }
+
 
                 }
             }

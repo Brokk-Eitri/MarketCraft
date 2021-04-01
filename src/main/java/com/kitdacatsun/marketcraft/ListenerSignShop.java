041837fc49a914697c01c919b760c65220ff0113
KitDaCatsun <kit.matthewson@gmail.com>
2021-04-01 12:06:17 +0100

    refactoring

--- src/main/java/com/kitdacatsun/marketcraft/ListenerSignShop.java
+++ src/main/java/com/kitdacatsun/marketcraft/ListenerSignShop.java
@@ -32,56 +32,51 @@ public class ListenerSignShop implements Listener {
         }
 
         WallSign sign = (WallSign) event.getClickedBlock();
-        String[] lines = ((Sign) sign).getLines();
-
-        if (!lines[0].toUpperCase().contains("SHOP")) {
-            return;
-        }
-
-        /*
-SHOP
-£15
-15 Spruce Logs
-KitDaCatsun
-         */
-
-        String notItemName = "\\d|[^a-zA-Z ]|[sS]\\Z";
-
-        int price = Integer.parseInt(lines[1].replaceAll("\\D", ""));
-        int amount = Integer.parseInt(lines[2].replaceAll("\\D", ""));
-        Material item = Material.getMaterial(lines[2].replaceAll(notItemName, ""));
-        Player seller = Bukkit.getServer().getPlayer(lines[3].replaceAll("\\W", ""));
-        Player buyer = event.getPlayer();
 
         Vector chestDirection = sign.getFacing().getOppositeFace().getDirection();
         Location chestLocation = ((Block) sign).getLocation().add(chestDirection);
-
-        if (seller == null) {
-            Bukkit.getLogger().warning("Could not find player " + lines[3] + " for chest shop at " + chestLocation);
-            return;
-        }
+        Inventory chest = ((Chest) chestLocation.getBlock()).getBlockInventory();
 
         if (!(chestLocation.getBlock() instanceof Chest)) {
             return;
         }
 
-        Inventory chest = ((Chest) chestLocation.getBlock()).getBlockInventory();
+        String[] lines = ((Sign)event.getClickedBlock()).getLines();
 
-        if (chest.getContents().length < 1) {
-            seller.sendMessage(ChatColor.YELLOW + "Your chest shop at " + chestLocation + " does not contain any items");
-            buyer.sendMessage(ChatColor.YELLOW + "There are no items in this chest");
+        if (!lines[0].toUpperCase().contains("SHOP")) {
+            return;
         }
 
-        ItemStack itemStack = new ItemStack(chest.getContents()[0]);
-        itemStack.setAmount(amount);
+        int price = extractInt(lines[1]);
 
-        if (!chest.containsAtLeast(itemStack, amount)) {
+        int amount = extractInt(lines[2]);
+        Material item = extractMaterial(lines[2]);
+        ItemStack itemStack = new ItemStack(item, amount);
+
+        Player seller = extractPlayer(lines[3]);
+        Player buyer = event.getPlayer();
+
+        if (!chest.containsAtLeast(itemStack, itemStack.getAmount())) {
             seller.sendMessage(ChatColor.YELLOW + "Your chest shop at " + chestLocation + " does not contain enough items");
             buyer.sendMessage(ChatColor.YELLOW + "This chest does not contain enough items");
+            return;
         }
 
-        seller.sendMessage(ChatColor.RED + "Items bought from chest shop at " + chestLocation);
-        buyer.sendMessage(ChatColor.GREEN + "You have bought " + amount + " " + )
+        seller.sendMessage(ChatColor.RED + "" + itemStack.getAmount() + " " + itemStack.getI18NDisplayName() + " bought from chest shop at " + chestLocation);
+        buyer.sendMessage(ChatColor.GREEN + "You have bought " + itemStack.getAmount() + " " + itemStack.getI18NDisplayName());
+
         CommandPay.pay(event.getPlayer(), seller, price);
+    }
+
+    private int extractInt(String string) {
+        return Integer.parseInt(string.replaceAll("\\D", ""));
+    }
+
+    private Material extractMaterial(String string) {
+        return Material.getMaterial(string.replaceAll("\\d|[^a-zA-Z ]|[sS]\\Z", ""));
+    }
+
+    private Player extractPlayer(String string) {
+        return Bukkit.getServer().getPlayer(string.replaceAll("\\W", ""));
     }
 }

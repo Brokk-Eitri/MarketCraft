b78e6b73063494c4dec5a92b71bec87d5627d54d
Jamie Kimber <jamiekimber@btinternet.com>
2021-08-02 21:35:35 +0100

    got crafting item change working

--- src/main/java/com/kitdacatsun/marketcraft/ListenerItemChange.java
+++ src/main/java/com/kitdacatsun/marketcraft/ListenerItemChange.java
@@ -4,20 +4,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.FurnaceSmeltEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerHarvestBlockEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 
 public class ListenerItemChange implements Listener {
@@ -47,29 +53,46 @@ public class ListenerItemChange implements Listener {
             return;
         }
 
-        for (Item item: event.getItems()) {
+        for (Item item : event.getItems()) {
             logItemChange(item.getItemStack());
         }
     }
 
     @EventHandler
     private void PlayerHarvestBlockEvent(PlayerHarvestBlockEvent event) {
-        for (ItemStack item: event.getItemsHarvested()) {
+        for (ItemStack item : event.getItemsHarvested()) {
             logItemChange(item.getType(), item.getAmount());
         }
     }
 
+
     @EventHandler
     private void CraftItemEvent(CraftItemEvent event) {
+        ItemStack craftedItem = event.getInventory().getResult();
+        Inventory inventory = event.getInventory();
+        ClickType clickType = event.getClick();
+        int outputAmount;
+        int lowerAmount;
+        if (clickType.isShiftClick()) {
+            lowerAmount = craftedItem.getMaxStackSize() + 1000;
+            for (ItemStack itemStack : inventory.getContents())
+                if (!itemStack.getType().isAir() && lowerAmount > itemStack.getAmount() && !itemStack.getType().equals(craftedItem.getType()))
+                    lowerAmount = itemStack.getAmount();
+        }else lowerAmount = 1;
+
+        outputAmount = lowerAmount * craftedItem.getAmount();
+        int inputAmount = outputAmount / event.getRecipe().getResult().getAmount();
+        logItemChange(craftedItem, outputAmount);
+
         for (ItemStack itemStack : event.getInventory().getMatrix()) {
-            //noinspection ConstantConditions
             if (itemStack != null) {
-                logItemChange(itemStack.getType(), -1);
+                logItemChange(itemStack.getType(), -1 * inputAmount);
             }
         }
 
-        logItemChange(Objects.requireNonNull(event.getInventory().getResult()));
     }
+
+
 
     @EventHandler
     private void FurnaceSmeltEvent(FurnaceSmeltEvent event) {

c1a58d4210b963ca1348cf21beb0c3debbfd86c3
Jamie Kimber <jamiekimber@btinternet.com>
2021-08-04 17:28:52 +0100

    fixed errors how kit told me to

--- src/main/java/com/kitdacatsun/marketcraft/ListenerItemChange.java
+++ src/main/java/com/kitdacatsun/marketcraft/ListenerItemChange.java
@@ -66,14 +66,10 @@ public class ListenerItemChange implements Listener {
         assert product != null;
         int crafted = 1;
         if (event.getClick().isShiftClick()) {
-            crafted = 64;
-            for (ItemStack reagent : event.getInventory().getMatrix()) {
-                if (reagent != null) {
-                    if (reagent.getAmount() < crafted) {
-                        crafted = reagent.getAmount();
-                    }
-                }
-            }
+            crafted = Arrays.stream(event.getInventory().getMatrix()).min((a, b) ->
+                    Math.min(a == null ? 64 : a.getAmount(),
+                            b == null ? 64 : b.getAmount())).orElseThrow().getAmount();
+
         }
 
         logItemChange(product, crafted * product.getAmount());

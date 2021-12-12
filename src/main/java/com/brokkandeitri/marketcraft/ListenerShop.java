9cfcff943b24c0b37aae6e197939b4cc89216a4c
KitDaCatsun <kit.matthewson@gmail.com>
2021-12-12 21:30:58 +0000

    Added grammar

--- src/main/java/com/brokkandeitri/marketcraft/ListenerShop.java
+++ src/main/java/com/brokkandeitri/marketcraft/ListenerShop.java
@@ -107,7 +107,7 @@ public class ListenerShop implements Listener {
 
                 files.balances.set(balanceKey, balances + cost);
 
-                player.sendMessage(ChatColor.GOLD + "You have sold " + order.getAmount() + " of " + order.getI18NDisplayName() + " for: £" + cost);
+                player.sendMessage(ChatColor.GOLD + "You have sold " + order.getAmount() + " " + order.getI18NDisplayName() + " £" + cost);
 
                 return;
 
@@ -122,7 +122,7 @@ public class ListenerShop implements Listener {
 
                         files.balances.set(balanceKey, balances - cost);
 
-                        player.sendMessage(ChatColor.GOLD + "You have bought " + order.getAmount() + " of " + order.getI18NDisplayName() + " for: £" + cost);
+                        player.sendMessage(ChatColor.GOLD + "You have bought " + order.getAmount() + " " + order.getI18NDisplayName() + " for £" + cost);
 
                     } else {
                         player.sendMessage(ChatColor.RED + "Not enough money to buy this item (Cost: £" + cost + ").");

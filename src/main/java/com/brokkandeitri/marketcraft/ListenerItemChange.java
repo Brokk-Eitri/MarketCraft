4473af7cae5cc2a81d1e7a8fb770c95a0a890a91
Jamie Kimber <jamiekimber@btinternet.com>
2021-12-14 22:10:41 +0000

    Removed redundant line

--- src/main/java/com/brokkandeitri/marketcraft/ListenerItemChange.java
+++ src/main/java/com/brokkandeitri/marketcraft/ListenerItemChange.java
@@ -19,7 +19,6 @@ import org.bukkit.event.player.PlayerHarvestBlockEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 

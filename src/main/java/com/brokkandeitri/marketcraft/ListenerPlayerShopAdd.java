ed868d4b95c4fc519325d630316cf392e8ae4d35
Jamie Kimber <jamiekimber@btinternet.com>
2021-12-26 22:00:51 +0000

    Initial price in auction is now the market price for the item

--- src/main/java/com/brokkandeitri/marketcraft/ListenerPlayerShopAdd.java
+++ src/main/java/com/brokkandeitri/marketcraft/ListenerPlayerShopAdd.java
@@ -82,6 +82,8 @@ public class ListenerPlayerShopAdd implements Listener {
         int price = 0;
         if (!Objects.requireNonNull(inventory.getItem(GUIBuilder.InvPos.BOT_MID)).getItemMeta().getDisplayName().equals("Select an option")) {
             price = Integer.parseInt(Objects.requireNonNull(inventory.getItem(GUIBuilder.InvPos.BOT_MID)).getItemMeta().getDisplayName().replaceAll("[^\\d.]", ""));
+        } else {
+            price = MarketCraft.getPrice(Objects.requireNonNull(inventory.getItem(GUIBuilder.InvPos.MID)));
         }
         return price;
     }
